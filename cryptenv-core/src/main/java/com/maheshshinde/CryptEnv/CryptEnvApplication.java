2d9d22d52496627c1650b1064d6fec8d2ed4c560
maheshshinde9100 <maheshshinde9100@gmail.com>
2026-07-19 21:13:51 +0530

    initial spring-boot project

---
+++ cryptenv-core/src/main/java/com/maheshshinde/CryptEnv/CryptEnvApplication.java
@@ -0,0 +1,13 @@
+package com.maheshshinde.CryptEnv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptEnvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptEnvApplication.class, args);
+	}
+
+}
---
+++ cryptenv-core/src/test/java/com/maheshshinde/CryptEnv/CryptEnvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maheshshinde.CryptEnv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptEnvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
